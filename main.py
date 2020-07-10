8c1df9d86f15ceabd68000462cab7d90162a5f7a
Anish Malla <63273493+Anish-Malla@users.noreply.github.com>
2020-07-11 02:15:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,42 @@
 import bs4
 import urllib.request
-import time
 import smtplib
-import pandas as pd
-from datetime import datetime
+import time
 
-url = 'https://www.amazon.in/FancyDressWale-Halloween-Inflatable-Tyrannosaurus-Performance/dp/B085RGBGLC/ref=sr_1_1?crid=345OCY4THDAGE&dchild=1&keywords=dinosaur+costume&qid=1593953482&sprefix=dinaosa%2Caps%2C266&sr=8-1'
-sauce = urllib.request.urlopen(url).read()
-soup = bs4.BeautifulSoup(sauce, "html.parser")
-print(soup)
+prices_list = []
 
-price_list = []
+def check_price():
+    url = 'https://www.amazon.in/FancyDressWale-Halloween-Inflatable-Tyrannosaurus-Performance/dp/B085RGBGLC/ref=sr_1_1?crid=1WFJNKSGHPAGC&dchild=1&keywords=dinosaur+costume+for+adults&qid=1594392785&sprefix=dinasour+costime%2Caps%2C292&sr=8-1'
 
-df = pd.DataFrame(columns=["Time", "Price"])
+    sauce = urllib.request.urlopen(url).read()
+    soup = bs4.BeautifulSoup(sauce, "html.parser")
 
-def price_decrease(price_list):
-    if price_list[-1] > price_list[-2]:
-        return False
-    else:
-        return True
+    prices = soup.find(id="priceblock_ourprice").get_text()
+    prices = float(prices.replace(",", "").replace("₹", ""))
+    prices_list.append(prices)
+    return prices
 
-def email(email_id, text):
+def send_email(message, sender_email, sender_password, receiver_email):
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
-    s.login("anishtesting1234@gmail.com", "Welcome123#ismypassword")
-    s.sendmail("anishtesting1234@gmail.com", email_id, text)
+    s.login(sender_email, sender_password)
+    s.sendmail(sender_email, receiver_email, message)
     s.quit()
 
-count = 0
+def price_decrease_check(price_list):
+    if prices_list[-1] < prices_list[-2]:
+        return True
+    else:
+        return False
+
+count = 1
 while True:
-    price = soup.find(id="priceblock_ourprice")
-    price = price.get_text()
-    price = float(price.replace(',', '').replace('₹', ''))
-    print(price)
-
-    if count >= 0 or price != price_list[-1]:
-        price_list.append(price)
-
-        df = df.append({"Time": datetime.now(), "Price": price}, ignore_index=True)
-        df.to_csv("price_list.csv")
-        if count >= 0:
-            if price_decrease(price_list):
-                print("THE PRICE WENT DOWN")
-                # print(text)
-                dif = price_list[-2] - price_list[-1]
-                message = f"PRICE WENT DOWN BY {dif} rupees. BUY NOW!"
-                # message = "HI MY NAME IS ANISH U DOG 123343"
-                email('mallasaianish@gmail.com', message)
-                print("sent")
-
-            else:
-                print("THE PRICE WENT UP")
-
+    current_price = check_price()
+    if count > 1:
+        flag = price_decrease_check(prices_list)
+        if flag:
+            decrease = prices_list[-1] - prices_list[-2]
+            message = f"The price has decrease please check the item. The price decrease by {decrease} rupees."
+            send_email(message) #ADD THE OTHER AGRUMENTS sender_email, sender_password, receiver_email
+    time.sleep(43000)
     count += 1
-
-    # time.sleep(21600)
-    time.sleep(10)
-
-# send_email(price_list, 'mallasaianish@gmail.com')
